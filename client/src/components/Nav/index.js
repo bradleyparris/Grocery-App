881406dda9d14b3beabae9a8da460d627762f8d9
buddy <buddylafluer@gmail.com>
2022-09-28 11:54:59 -0400

    working on integrating react pages

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Nav(){
+    
+    return (
+        <header>
+            <h1>Paradise Produce</h1>
+            <nav>
+
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
